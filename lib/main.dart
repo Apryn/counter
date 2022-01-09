5ecd03565e1ea15f928c6d5c750a35d02a9e029c
Apryn <apriyanriyan23@gmail.com>
2022-01-09 12:07:20 +0700

    Anonymous Method

--- lib/main.dart
+++ lib/main.dart
@@ -3,52 +3,46 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  int number = 1;
-  int reset = 1;
-  void tekantombol() {
-    setState(() {
-      number = number + 1;
-    });
-  }
-
-  void clear() {
-    setState(() {
-      number = reset;
-    });
-  }
-
+  int number = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Statefull Widget"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Statefull Widget'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(number.toString()),
+              RaisedButton(
+                child: Text('Tambah Bilangan'),
+                onPressed: () {
+                  setState(() {
+                    number = number + 1;
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text('Reset'),
+                onPressed: () {
+                  setState(() {
+                    number;
+                  });
+                },
+              )
+            ],
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  number.toString(),
-                  style: TextStyle(fontSize: 1 + number.toDouble()),
-                ),
-                RaisedButton(
-                  child: Text('Tambah'),
-                  onPressed: tekantombol,
-                ),
-                RaisedButton(
-                  child: Text('Reset'),
-                  onPressed: clear,
-                )
-              ],
-            ),
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
